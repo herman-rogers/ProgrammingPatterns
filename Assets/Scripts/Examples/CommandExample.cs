d1793c819ef3b72eaab09befcffa17040166e98b
Boomer Rogers <herman.rogers@gmail.com>
2015-01-13 13:47:44 +0000

    Adding in the command pattern

---
+++ Assets/Scripts/Examples/CommandExample.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CommandExample : MonoBehaviour
+{
+    public void Message( )
+    {
+        Debug.Log( "Command Message" );
+    }
+}
